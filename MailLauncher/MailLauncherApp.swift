65d4eb4707c460f5985eee0ebcfd6f2827f19b81
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-06-24 22:12:46 +0700

    Initial Commit

---
+++ MailLauncher/MailLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailLauncherApp.swift
+//  MailLauncher
+//
+//  Created by Анастасия Конончук on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MailLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
